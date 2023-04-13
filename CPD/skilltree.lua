4030f0d6d73f57ecaa590a34c1d18d6defb56cef
Kowaak <102755037+Kowaak@users.noreply.github.com>
2023-04-13 13:06:59 +0200

    Update skilltree.lua

--- CPD/skilltree.lua
+++ CPD/skilltree.lua
@@ -38,45 +38,45 @@ local pc7 = 6000
 local pc9 = 10000
 local pdcb = "player_passive_loot_drop_multiplier"
 table.insert(self.specializations,{
-name_id = "cock01'name",
-desc_id = "cock01'desc",{
+name_id = "perk01'name",
+desc_id = "prek01'desc",{
 upgrades = {
 "player_passive_damage_reduction_1", "team_damage_reduction_1","player_melee_kill_life_leech", "player_melee_life_leech", "melee_stacking_hit_damage_multiplier_1", "melee_stacking_hit_expire_t"
 },
 cost = pc1,
 icon_xy = {1, 1},
-name_id = "cock01'perk1n",
-desc_id = "cock01'perk1d"},
+name_id = "perk01'perk1n",
+desc_id = "perk01'perk1d"},
 ps2,{
 upgrades = {
 "player_passive_health_multiplier_4","player_passive_health_regen","player_panic_suppression","temporary_loose_ammo_restore_health_3","player_loose_ammo_restore_health_give_team","temporary_loose_ammo_give_team"
 },
 cost = pc3,
 icon_xy = {1, 1},
-name_id = "cock01'perk3n",
-desc_id = "cock01'perk3d"},
+name_id = "perk01'perk3n",
+desc_id = "perk01'perk3d"},
 ps4,{
 upgrades = {
 "player_passive_dodge_chance_1","player_movement_speed_damage_health_ratio_threshold_multiplier","player_armor_regen_damage_health_ratio_threshold_multiplier",
 },
 cost = pc5,
 icon_xy = {1, 1},
-name_id = "cock01'perk5n",
-desc_id = "cock01'perk5d"},
+name_id = "perk01'perk5n",
+desc_id = "perk01'perk5d"},
 ps6,{
 upgrades = {
 "player_armor_increase_1","player_perk_armor_regen_timer_multiplier_5"
 },
 cost = pc7,
 icon_xy = {1, 1},
-name_id = "cock01'perk7n",
-desc_id = "cock01'perk7d"},
+name_id = "perk01'perk7n",
+desc_id = "perk01'perk7d"},
 ps8,{
 upgrades = {
 "team_xp_multiplier","player_damage_to_armor_1","player_kill_change_regenerate_speed","player_dodge_replenish_armor"
 },
 cost = pc9,
 icon_xy = {1, 1},
-name_id = "cock01'perk9n",
-desc_id = "cock01'perk9d"}})
+name_id = "perk01'perk9n",
+desc_id = "perk01'perk9d"}})
 end
